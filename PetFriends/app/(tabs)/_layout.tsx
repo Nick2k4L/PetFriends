754495251c7ed4d7c7d527e675992a641005d73a
Sean <wackowookiee@outlook.com>
2024-11-17 00:19:04 -0800

    Update _layout.tsx

--- PetFriends/app/(tabs)/_layout.tsx
+++ PetFriends/app/(tabs)/_layout.tsx
@@ -2,13 +2,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../LoginScreen';
 import PetManagementScreen from '../PetManagementScreen';
 import Siper from '../Siper';
+import Home from './index'
 const Stack = createStackNavigator();
 
 export default function TabLayout() {
   return (
-    <Stack.Navigator initialRouteName="Login" screenOptions={{
+    <Stack.Navigator initialRouteName="Home" screenOptions={{
       headerShown: false
     }}>
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="PetManagement" component={PetManagementScreen} />
       <Stack.Screen name="DogSwiper" component={Siper} />
